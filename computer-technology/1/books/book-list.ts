0e886968f1310ddd99afa06ed3704eb98cb6146b
Maksym Taleynikov <blakphantom2@gmail.com>
2025-03-10 10:51:05 +0200

    Rebuild

---
+++ computer-technology/1/books/book-list.ts
@@ -0,0 +1,65 @@
+import { Good } from '../../../../../core-library/src/lib/component/shop-list/good.interface';
+import { createGoodList } from '../../../../../core-library/src/lib/component/shop-list/adapter';
+
+export const BOOK_LIST: Good[] = createGoodList([
+    {
+        label: 'All About Laptop Computer Parts',
+        price: 1953
+    },
+    {
+        label: 'Computer Components',
+        price: 2353
+    },
+    {
+        label: 'Computer Data Recovery',
+        price: 4053
+    },
+    {
+        label: 'Computer Data Recovery Vol. 3',
+        price: 4053
+    },
+    {
+        label: 'Computer Hard Drive Data Recovery',
+        price: 4053
+    },
+    {
+        label: 'Computer Hardware',
+        price: 4053
+    },
+    {
+        label: 'Computer IT Training',
+        price: 4053
+    },
+    {
+        label: 'Computer Recycling',
+        price: 4053
+    },
+    {
+        label: 'Computer Training Job',
+        price: 4053
+    },
+    {
+        label: 'Computers & Technology',
+        price: 4053
+    },
+    {
+        label: 'Guide to Computer Architecture',
+        price: 4053
+    },
+    {
+        label: 'IT Essentials And Data Recovery For Online Businesses',
+        price: 4053
+    },
+    {
+        label: 'It`s a Mac',
+        price: 4053
+    },
+    {
+        label: 'Laptop Computer Parts',
+        price: 4053
+    },
+    {
+        label: 'What is Computer Monitor',
+        price: 4053
+    }
+]);
